9fa0391a4be5f685749df09c2426523a3e0e67f1
PotatoHuang <stevenhuang.dog@gmail.com>
2026-04-15 22:29:36 +0800

    comment in submission.c

--- worker/listener.c
+++
@@ -1,7 +0,0 @@
-#include "../config.c"
-#include "../include/config.h"
-#include "../include/sandbox_path.h"
-
-int main()
-{
-}
--- worker/submission.c
+++ worker/submission.c
@@ -16,13 +16,13 @@
  * *err_code: when return array == NULL. Check this value. If return isn't NULL, this value should write as judge_accept
  */
 
-#define L_LIMIT_FSIZE_MB 1 // limit that used for limit maximum file size. write with rlimit, function will multiply 1024 * 1024
-#define L_LIMIT_NPROC 1
-#define PROBLEM_CONFIG_CASE_COUNT "case_count=%d"
-#define PROBLEM_CONFIG_CPU "limit_time_s=%d"
-#define PROBLEM_CONFIG_AS "limit_as_mb=%d"
-#define PROBLEM_CONFIG_STACK "limit_stack_mb=%d"
-#define PROBLEM_CONFIG_MAX_RES "expect_max_result_mb=%lld"
+#define L_LIMIT_FSIZE_MB 1 // limit FSIZE
+#define L_LIMIT_NPROC 1 // limit NPROC
+#define PROBLEM_CONFIG_CASE_COUNT "case_count=%d"		// case count // these should match problem/X/config.conf
+#define PROBLEM_CONFIG_CPU "limit_time_s=%d"			// cpu time limit
+#define PROBLEM_CONFIG_AS "limit_as_mb=%d"			// memory limit
+#define PROBLEM_CONFIG_STACK "limit_stack_mb=%d"		// stack limit
+#define PROBLEM_CONFIG_MAX_RES "expect_max_result_mb=%lld"	// maximum result expect size (using for set shared memory)
 
 judge_result_t *submission(const judge_task_t *task, int *ret_case_count, judge_status_t *err_code)
 {
